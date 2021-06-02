b3185c77f067370723af313e6f750a762312288b
EkaterinaT <tarakanova_ekaterina@hotmail.com>
2021-06-02 14:24:31 +0300

    add tab trap for modal dialog

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './modal.module.css';
 import {CloseIcon}  from '@ya.praktikum/react-developer-burger-ui-components';
@@ -7,25 +7,55 @@ import PropTypes from 'prop-types';
 
 const Modal = (props) => {
 
+    //input for tab trap inside modal
+    const [activeBeforeModal, setActiveBeforeModal] = useState(null);
+    const [modalControls, setModalControls] = useState([]);
+
     const { children, onClose, type, modalComponent} = props;
     const modalRoot = document.querySelector("#modals");
 
     React.useEffect(() => {
-            modalComponent.current.focus();
-        }
-    );
 
-    const onEscape = (e) => {
-        if(e.code === "Escape") {
-            onClose(e);
+        setActiveBeforeModal(document.activeElement);
+        setModalControls([modalComponent.current, modalComponent.current.querySelector('button')]);
+
+        modalComponent.current.focus();
+
+    }, [modalComponent]);
+
+    const tabTrap = (e) => {
+                
+        if(e.code === "Tab") {            
+            const activeElement = document.activeElement;
+            
+            if(e.shiftKey && activeElement === modalControls[0]) {
+                e.preventDefault();
+                modalControls[modalControls.length-1].focus();
+            }
+
+            if(activeElement === modalControls[modalControls.length-1]) {
+                e.preventDefault();
+                modalControls[0].focus();
+            } 
         }
     }
-  
+
+    const handleKeyDown = (e) => {
+
+        if(e.code === "Escape") {
+            activeBeforeModal.focus();
+            onClose(e);            
+        }
+
+        //tab trap for modal dialog
+        tabTrap(e);
+    }
+
     return ReactDOM.createPortal(
         (
             <>
-                <article className={styles.modal} tabIndex={0} ref={modalComponent} onKeyDown={onEscape}>
-                    <div className={styles["close_btn_"+type]}><CloseIcon onClick={onClose}/></div>
+                <article className={styles.modal} tabIndex={0} ref={modalComponent} onKeyDown={handleKeyDown}>
+                    <button className={styles["close_btn_"+type]} onClick={onClose}><CloseIcon/></button>
                     {children}
                 </article>
                 <ModalOverlay onClose={onClose}/>
