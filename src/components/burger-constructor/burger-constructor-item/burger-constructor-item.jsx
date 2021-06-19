b7058b0de135a52a6ba7ad41b57be4a7d0da97e8
Ekaterina-T <tarakanova_ekaterina@hotmail.com>
2021-06-19 16:36:47 +0300

    drag and drop on hover

--- src/components/burger-constructor/burger-constructor-item/burger-constructor-item.jsx
+++ src/components/burger-constructor/burger-constructor-item/burger-constructor-item.jsx
@@ -13,8 +13,10 @@ import {isImageLink} from '../../../utils/prop-type-custom-checks'
 const BurgerConstructorItem = ({index, id, text, price, thumbnail, isLocked}) => {
     
     const dispatch = useDispatch();
+
+    const ref = React.useRef(null);
     
-    const [collectedDrag, dragRef] = useDrag({
+    const [{isDragging}, drag] = useDrag({
         type: "fillings", 
         item: {id, index},
         collect: monitor => ({
@@ -22,15 +24,35 @@ const BurgerConstructorItem = ({index, id, text, price, thumbnail, isLocked}) =>
         })
     });
 
-    const [, fillingsRef] = useDrop({
+    const [, drop] = useDrop({
         accept:'fillings',
-        drop(item) {
-            const draggedIndex = item.index;
+        hover(item, monitor) {
+
+            const dragIndex = item.index;
             const hoverIndex = index;
 
-            if(draggedIndex !== hoverIndex) {
-                dispatch(sortFillingsOrder(draggedIndex, hoverIndex));
-            }            
+            if(dragIndex === hoverIndex) {
+                return;
+            }
+
+            const node = ref.current.querySelector('div');
+            const hoverBoundingRect = node.getBoundingClientRect();            
+            const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+            const clientOffset = monitor.getClientOffset(); //cursor position when it intersects drop target
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) { //downwards
+                return;
+            }
+
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) { //upwards
+                return;
+            }
+
+            dispatch(sortFillingsOrder(dragIndex, hoverIndex));
+            console.log(monitor.getItem())
+            monitor.getItem().index = hoverIndex;
         }
     });
 
@@ -38,21 +60,21 @@ const BurgerConstructorItem = ({index, id, text, price, thumbnail, isLocked}) =>
         dispatch(removeIngredientFromCart(ingredientKey));
     }
 
+    const style = { cursor: "grabbing"};
+    const opacity = isDragging ? 0 : 1;
+
+    drag(drop(ref));
+
     return (
-        <div ref={fillingsRef}>
-            {!collectedDrag.isDragging ?   
-                <div id={id} className={styles.burgerIngredient} ref={dragRef}  >
-                    <div className={styles.dragIcon}><DragIcon type="primary" /></div>
-                    <ConstructorElement
-                    isLocked={isLocked}
-                    text={text}
-                    price={price}
-                    thumbnail={thumbnail}
-                    handleClose={() => removeIngredient(id)}/>
-                </div>
-                : <div className={styles.dragPreview} ></div>
-            }
-        </div>
+            <div id={id} className={styles.burgerIngredient} ref={ref}  style={{ ...style, opacity}}>
+                <div className={styles.dragIcon}><DragIcon type="primary" /></div>
+                <ConstructorElement
+                isLocked={isLocked}
+                text={text}
+                price={price}
+                thumbnail={thumbnail}
+                handleClose={() => removeIngredient(id)}/>
+            </div>
     )
 
 }
