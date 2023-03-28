08540393ab293cf9ec6c7fac53eef02872a65190
EkaterinaT <tarakanova_ekaterina@hotmail.com>
2023-03-28 23:06:04 +0300

    DragNDrop bug: move ingridient link inside the component

--- src/components/burger-ingredients/ingredient/ingredient.tsx
+++ src/components/burger-ingredients/ingredient/ingredient.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { useDrag } from 'react-dnd';
+import { Link, useLocation } from 'react-router-dom';
 
 import { Counter, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient.module.css';
@@ -15,6 +16,7 @@ interface IIngredient {
 }
 
 function Ingredient({ data, openModal }: IIngredient) {
+  const location = useLocation();
   const dispatch = useAppDispatch();
   const { bun, fillings } = useAppSelector((store) => store.cart);
   const [, dragRef] = useDrag({ type: 'ingredient', item: { id: data._id, type: data.type } });
@@ -46,18 +48,26 @@ function Ingredient({ data, openModal }: IIngredient) {
     // eslint-disable-next-line max-len
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <li className={styles.card} onClick={openIngredientDetails} ref={dragRef}>
+      {
+        /*
+          Link must be inside dragRef element due to React DnD bug:
+          https://github.com/react-dnd/react-dnd/issues/869#issuecomment-502688377
+        */
+      }
+      <Link key={data._id} to={{ pathname: `/ingredients/${data._id}`, state: { background: location } }}>
 
-      { ingredientCount > 0 && <Counter count={ingredientCount} size="default" /> }
-      <img src={image} alt={name} className={styles.image} />
-      <div className={styles.price}>
-        <span className={styles.price_num}>{price}</span>
-        <CurrencyIcon type="secondary" />
-      </div>
-      <div className={styles.name}>{name}</div>
-      <div className={styles.add_btn}>
-        <Button type="secondary" size="medium" onClick={addIngredientViaButton}>Добавить</Button>
-      </div>
+        { ingredientCount > 0 && <Counter count={ingredientCount} size="default" /> }
+        <img src={image} alt={name} className={styles.image} />
+        <div className={styles.price}>
+          <span className={styles.price_num}>{price}</span>
+          <CurrencyIcon type="secondary" />
+        </div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.add_btn}>
+          <Button type="secondary" size="medium" onClick={addIngredientViaButton}>Добавить</Button>
+        </div>
 
+      </Link>
     </li>
   );
 }
